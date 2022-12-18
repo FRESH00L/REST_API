6e36951b3b752a40ff02ccfc4d7c392d26dc649a
FRESH00L <115614901+FRESH00L@users.noreply.github.com>
2022-12-18 15:31:56 +0100

    configure people requests

--- app/Http/Requests/StorePeopleRequest.php
+++ app/Http/Requests/StorePeopleRequest.php
@@ -13,7 +13,7 @@ class StorePeopleRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class StorePeopleRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'Name' => ['required'],
+            'LastName' => ['required']
         ];
     }
 }
